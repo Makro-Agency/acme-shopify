10e252124e339ca925a57f47d26cb40bc25a1048
jakeMakro <154862045+jakeMakro@users.noreply.github.com>
2025-05-06 20:46:46 +0530

    ACME-160

---
+++ assets/custom.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.querySelectorAll('.qty-btn').forEach((quantityBtn) => {
+    quantityBtn.addEventListener('click',function(){
+      var get_val = parseInt(quantityBtn.dataset.val);
+      if(quantityBtn.classList.contains('qty-dec')){
+        get_val = get_val - 1;
+        quantityBtn.closest('.product-qty').querySelector('.qty-dec').setAttribute('data-val',get_val);
+        quantityBtn.closest('.product-qty').querySelector('.qty-inc').setAttribute('data-val',get_val);
+        if(get_val == 1){
+          quantityBtn.closest('.product-qty').querySelector('.qty-dec').classList.add('disabled');
+        }
+      }else{
+        get_val = get_val + 1;
+        quantityBtn.closest('.product-qty').querySelector('.qty-dec').setAttribute('data-val',get_val);
+        quantityBtn.closest('.product-qty').querySelector('.qty-inc').setAttribute('data-val',get_val);
+        if(get_val > 1){
+          quantityBtn.closest('.product-qty').querySelector('.qty-dec').classList.remove('disabled');
+        }
+      }
+    })
+  })
+});
